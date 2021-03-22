5dcbc378f658ac95a0b2af559da5330a80c3e9ac
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2021-03-22 14:31:23 +0100

    clientreceiver

--- src/client/mediator/ClientReceiver.java
+++ src/client/mediator/ClientReceiver.java
@@ -1,4 +1,44 @@
 package client.mediator;
 
-public class ClientReceiver {
+import com.google.gson.Gson;
+import server.model.Message;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ClientReceiver implements Runnable {
+    private ChatClient client;
+    private BufferedReader in;
+    private boolean connected;
+    private Gson gson;
+    
+    public ClientReceiver(ChatClient client, BufferedReader in) {
+        this.client = client;
+        this.in = in;
+        connected = true;
+        gson = new Gson();
+    }
+    
+    public void disconnect() {
+        connected = false;
+    }
+    
+    @Override
+    public void run() {
+        while (connected) {
+            try {
+                Message message = gson.fromJson(in.readLine(), Message.class);
+                client.received(message);
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            in.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
