d1ee1ee4d447b89fe75179c6b324dccaa25bf8d6
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2021-03-22 13:44:29 +0100

    yay

--- src/client/view/LoginViewController.java
+++ src/client/view/LoginViewController.java
@@ -1,9 +1,14 @@
 package client.view;
 
 import client.viewmodel.LoginViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class LoginViewController extends ViewController {
     private LoginViewModel _viewModel;
+    
+    @FXML
+    private TextField usernameInput;
     
     public LoginViewController() {
     
@@ -12,10 +17,16 @@ public class LoginViewController extends ViewController {
     @Override
     protected void init() {
         _viewModel = getViewModelFactory().getLoginViewModel();
+        usernameInput.textProperty().bindBidirectional(_viewModel.getUsernameProperty());
     }
     
     @Override
     public void reset() {
+        _viewModel.reset();
+    }
     
+    @FXML
+    private void connect() {
+        getViewHandler().openView(View.CHATVIEW);
     }
 }
--- src/client/viewmodel/LoginViewModel.java
+++ src/client/viewmodel/LoginViewModel.java
@@ -1,11 +1,23 @@
 package client.viewmodel;
 
 import client.model.Model;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
 public class LoginViewModel {
     private Model _model;
+    private StringProperty _username;
     
     public LoginViewModel(Model model) {
         _model = model;
+        _username = new SimpleStringProperty();
+    }
+    
+    public StringProperty getUsernameProperty() {
+        return _username;
+    }
+    
+    public void reset() {
+        _username.set("");
     }
 }
