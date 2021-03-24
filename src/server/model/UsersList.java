d37a7a8b733d4356223e35e13c3da882db3b62af
stefaniatomuta <304173@via.dk>
2021-03-24 12:37:08 +0100

    gr45

--- src/server/model/UsersList.java
+++ src/server/model/UsersList.java
@@ -5,12 +5,17 @@ import java.util.ArrayList;
 public class UsersList
 {
     private ArrayList<String> usersList;
+    private String type;
 
-    public UsersList()
+    public UsersList(String type, ArrayList<String> usersList)
     {
-        this.usersList = new ArrayList<>();
+        this.type = type;
+        this.usersList = usersList;
     }
     public ArrayList<String> getUsersList(){
         return usersList;
+    }
+    public String getType(){
+        return type;
     }
 }
